b13f4c0f5fb43cfa45a20e9bc3d6dcba2f387f54
Adrian <marin.mag@stx.ox.ac.uk>
2023-06-21 22:38:46 +0100

    removed from git tracking all but codes

--- obspyfy_simulation.py
+++ obspyfy_simulation.py
@@ -2,5 +2,5 @@ import sys
 sys.path.append('/home/users/scro4564/PhD/AxiSEM3D_Data_Handler')
 from AxiSEM3D_Data_Handler.obspyfy import obspyfy
 
-obspyfy('/disks/data/PhD/CMB/simu3D_CMB/NORMAL_FAULT_100KM_CMB_50_0_30_30', output_type='stations', 
+obspyfy('/disks/data/PhD/CMB/simu1D/FORWARD_CHECK', output_type='stations', 
         stations_paths='/disks/data/PhD/CMB/stations/STA_10DEG_CROSS.txt')                 
